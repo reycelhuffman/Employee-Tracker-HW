452ba7a811236b244bf1f766a49615a782321fa9
Reycel Huffman <reycel.huffman@outlook.com>
2022-05-01 19:25:36 -0500

    save schema work

--- example.js
+++ example.js
@@ -1,6 +1,6 @@
 const { prompt } = require("inquirer");
 const mysql = require('mysql2/promise');
-let db;
+let employeedb;
   
 awaitMySqlWithInquirer();
 
---
+++ schema.sql
@@ -0,0 +1,28 @@
+DROP DATABASE IF EXISTS employees_db;
+CREATE DATABASE employees_db;
+
+USE employees_db;
+
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE employee_role (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary VARCHAR(30) NOT NULL,
+    department_id  INT,
+    FOREIGN KEY (department)
+    REFERENCES department(id)
+    ON DELETE SET NULL
+);
+
+CREATE TABLE employee (
+    id  INT NOT NULL AUNTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT,
+    manger_id INT 
+
+)
