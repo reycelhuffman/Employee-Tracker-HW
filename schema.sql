2b84f19d6dad0559cd4020f1279a994e1eb2db09
Reycel Huffman <reycel.huffman@outlook.com>
2022-05-01 21:26:23 -0500

    got schema working

--- schema.sql
+++ schema.sql
@@ -11,21 +11,15 @@ CREATE TABLE department (
 CREATE TABLE employee_role (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
-    salary VARCHAR(30) NOT NULL,
-    department_id  INT,
-    FOREIGN KEY (department)
-    REFERENCES department(id)
-    ON DELETE SET NULL
+    salary DECIMAL NOT NULL,
+    department_id  INT NOT NULL
 );
 
 CREATE TABLE employee (
-    id  INT NOT NULL AUNTO_INCREMENT PRIMARY KEY,
+    id  INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INT,
-    manager_id INT,
-    FOREIGN KEY (role, manager)
-    REFERENCES role, manager(id)
-
+    role_id INT NOT NULL,
+    manager_id INT NOT NULL
 
 );
