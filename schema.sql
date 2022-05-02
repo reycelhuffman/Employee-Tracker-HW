c445d4e3404c8652f3810cdfb57c1123478d09d5
Reycel Huffman <reycel.huffman@outlook.com>
2022-05-01 19:46:04 -0500

    deleted query

--- schema.sql
+++ schema.sql
@@ -23,6 +23,9 @@ CREATE TABLE employee (
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT,
-    manger_id INT 
+    manager_id INT,
+    FOREIGN KEY (role, manager)
+    REFERENCES role, manager(id)
 
-)
+
+);
